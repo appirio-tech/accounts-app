9191a30c3f1ee75576b261a169fff29ca9ebcc33
Andrew Selbie <aselbie@gmail.com>
2016-04-14 16:19:28 -0700

    Cleanup util function a little

--- core/utils.js
+++ core/utils.js
@@ -1,5 +1,8 @@
 import { TC_JWT, ZENDESK_JWT, DOMAIN, ZENDESK_DOMAIN } from './constants.js'
 import { getToken } from './token.js'
+import pickBy from 'lodash/pickBy'
+import mapValues from 'lodash/mapValues'
+import fromPairs from 'lodash/fromPairs'
 
 export function getLoginConnection(userId) {
   return isEmail(userId) ? 'TC-User-Database' : 'LDAP'
@@ -15,45 +18,26 @@ export function isUrl(value) {
   const URL_PATTERN = /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?/i;
 
   return URL_PATTERN.test(value);
-};
+}
 
 export function encodeParams(params, includeNull) {
-  const result = {}
+  const relevantParams = pickBy(params, v => v || includeNull )
 
-  for (p in params) {
-    v = params[p]
-
-    if (v || includeNull) {
-      result[p] = encodeURIComponent(v)
-    }
-  }
-
-  return result
+  return mapValues(relevantParams, v => encodeURIComponent(v) )
 }
 
 export function parseQuery(query) {
-  const params = {}
+  const params = query.split('&').map( param => {
+    const [ k, v ] = param.split('=')
 
-  function parseKV(kv) {
-    var pair
-    pair = kv.split('=')
-    if (pair.length === 1) {
-      return params[pair[0]] = null
-    } else {
-      return params[pair[0]] = decodeURIComponent(pair[1])
-    }
-  }
+    return [ k, v ? decodeURIComponent(v) : null ]
+  })
 
-  ref1 = query.split('&')
-  for (i = 0, len = ref1.length; i < len; i++) {
-    kv = ref1[i]
-    parseKV(kv)
-  }
-  return params
+  return fromPairs(params)
 }
 
 export function setupLoginEventMetrics(id) {
-  if ($window._kmq) {
-    return $window._kmq.push(['identify', id])
+  if (window._kmq) {
+    return window._kmq.push(['identify', id])
   }
 }
