dd429c8ac5fc17b46413ff178e27f73c9c2044ba
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2020-04-16 14:27:13 +0530

    rs256 lib

--- exports.js
+++ exports.js
@@ -1,2 +1,3 @@
-export { configureConnector, getFreshToken, logout } from './connector/connector-wrapper.js'
-export { isTokenExpired, decodeToken } from './core/token.js'
+//export { configureConnector, getFreshToken, logout } from './connector/connector-wrapper.js'
+//export { isTokenExpired, decodeToken } from './core/token.js'
+export { isTokenExpired, decodeToken, getFreshToken } from './core/idtoken.js'
